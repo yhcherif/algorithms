2dd8254a7c86985fface6dd35c57b784c5149498
Youssouf Cherif <youssouf@codebreaks.io>
2020-03-12 22:56:45 +0100

    Add better linear search algorithm

---
+++ Searching/linear/better-linear.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * Better Linear search implementation
+ * Inputs:
+ *     array: the elements we are searching a given element in.
+ *     size: the size of the array.
+ *     x: the element we searching its presence in the array.
+ * Runnig time:
+ *     - Best-case: O(1); When the element we are searching is ths first element in the  array.
+ *     - Worst-case: O(n) for n this size of the array. When the ellement we are searching is the last element in the array.
+ * Requires the array to be sorted? No.
+ */
+
+const search = (array, size, x) => {
+    for (let i = 0; i <= size; i++) {
+        if(array[i] == x){
+            return i;
+        }
+    }
+
+    return null;
+}
+
+
+module.exports = search;
